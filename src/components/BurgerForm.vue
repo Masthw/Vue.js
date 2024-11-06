c0e33fc2754778da5298e14b11bdb759531a5881
Matheus dos Santos Soares <maverick17xd@gmail.com>
2024-11-06 11:03:28 -0300

    feat: fetch data for burger form directly from backend

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -17,46 +17,32 @@
           <label for="pao">Escolha o pão:</label>
           <select name="pao" id="pao" v-model="pao">
             <option value="">Selecione o seu pão</option>
-            <option value="integral">Integral</option>
+            <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+              {{ pao.tipo }} 
+            </option>
           </select>
         </div>
         <div class="input-container">
           <label for="carne">Escolha a carne do seu Burger:</label>
           <select name="carne" id="carne" v-model="carne">
             <option value="">Selecione o tipo de carne</option>
-            <option value="maminha">Maminha</option>
+            <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+              {{ carne.tipo }} 
+            </option>
           </select>
         </div>
         <div id="opcionais-container" class="input-container">
           <label id="opcionais-title" for="opcionais"
             >Selecione os opcionais:</label
           >
-          <div class="checkbox-container">
+          <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
             <input
               type="checkbox"
               name="opcionais"
               v-model="opcionais"
-              value="salame"
+              :value="opcional.tipo"
             />
-            <span>Salame</span>
-          </div>
-          <div class="checkbox-container">
-            <input
-              type="checkbox"
-              name="opcionais"
-              v-model="opcionais"
-              value="salame"
-            />
-            <span>Salame</span>
-          </div>
-          <div class="checkbox-container">
-            <input
-              type="checkbox"
-              name="opcionais"
-              v-model="opcionais"
-              value="salame"
-            />
-            <span>Salame</span>
+            <span>{{ opcional.tipo }}</span>
           </div>
           <div class="input-container">
             <input type="submit" class="submit-btn" value="Criar meu Burger!" />
@@ -70,6 +56,31 @@
 <script>
 export default {
   name: "BurgerForm",
+  data() {
+    return {
+      paes: null,
+      carnes: null,
+      opcionaisdata: null,
+      nome: null,
+      pao: null,
+      opcionais: [],
+      status: "Solicitado",
+      msg: null,
+    };
+  },
+  methods: {
+    async getIngredientes() {
+      const req = await fetch("http://localhost:3000/ingredientes");
+      const data = await req.json();
+
+      this.paes = data.paes;
+      this.carnes = data.carnes;
+      this.opcionaisdata = data.opcionais;
+    },
+  },
+  mounted() {
+    this.getIngredientes();
+  },
 };
 </script>
 
@@ -115,26 +126,26 @@ select {
 
 .checkbox-container span,
 .checkbox-container input {
-    width: auto;
+  width: auto;
 }
 .checkbox-container span {
-    margin-left: 6px;
-    font-weight: bold;
+  margin-left: 6px;
+  font-weight: bold;
 }
 .submit-btn {
-    background-color: #222;
-    color: #fcba03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
 }
 
 .submit-btn:hover {
-    background-color: transparent;
-    color: #222;
+  background-color: transparent;
+  color: #222;
 }
 </style>
