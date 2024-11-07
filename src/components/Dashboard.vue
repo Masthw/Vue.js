06c239381d49762470351af7a32069a2aa97a15b
Matheus dos Santos Soares <maverick17xd@gmail.com>
2024-11-07 08:31:22 -0300

    feat: fetch order data directly from database

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,20 +11,23 @@
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
           <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">
+              {{ opcional }}
+            </li>
           </ul>
         </div>
         <div>
           <select name="status" id="status">
-            <option value="">Selecione</option>
+            <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">
+              {{ s.tipo }}
+            </option>
           </select>
           <button class="delete-btn">Cancelar</button>
         </div>
@@ -36,64 +39,91 @@
 <script>
 export default {
   name: "Dashboard",
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+    };
+  },
+  methods: {
+    async getPedidos() {
+      const req = await fetch("http://localhost:3000/burgers");
+
+      const data = await req.json();
+
+      this.burgers = data;
+
+      this.getStatus();
+    },
+    async getStatus() {
+      const req = await fetch("http://localhost:3000/status");
+
+      const data = await req.json();
+
+      this.status = data;
+    },
+  },
+  mounted() {
+    this.getPedidos();
+  },
 };
 </script>
 
 <style scoped>
 #burger-table {
-    max-width: 1200px;
-    margin: 0 auto;
+  max-width: 1200px;
+  margin: 0 auto;
 }
 
-#burger-table-heading, 
+#burger-table-heading,
 #burger-table-rows,
 .burger-table-row {
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 }
 
 #burger-table-heading {
-    font-weight: bold;
-    padding: 12px;
-    border-bottom: 3px solid #333;
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
 }
 
 #burger-table-heading div,
 .burger-table-row div {
-    width: 19%;
+  width: 19%;
 }
 
 .burger-table-row {
-    width: 100%;
-    padding: 12px;
-    border-bottom: 1px solid #ccc;
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
 }
 
 #burger-table-heading .order-id,
 .burger-table-row .order-number {
-    width: 5%;
+  width: 5%;
 }
 
 select {
-    padding: 12px 6px;
-    margin-right: 12px;
+  padding: 12px 6px;
+  margin-right: 12px;
 }
 
 .delete-btn {
-    background-color: #222;
-    color: #FCBA03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
 }
 
 .delete-btn:hover {
-    background-color: transparent;
-    color: #222;
+  background-color: transparent;
+  color: #222;
 }
-
 </style>
